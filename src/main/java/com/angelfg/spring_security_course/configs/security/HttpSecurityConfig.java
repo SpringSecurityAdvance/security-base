09b248023d09b42de156a5a1faed535b11c03591
Luis Angel <angelfgdeveloper@gmail.com>
2025-04-19 14:24:44 -0600

    Autorizacion en endpoints con authorities

--- src/main/java/com/angelfg/spring_security_course/configs/security/HttpSecurityConfig.java
+++ src/main/java/com/angelfg/spring_security_course/configs/security/HttpSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.angelfg.spring_security_course.configs.security;
 
 import com.angelfg.spring_security_course.configs.filters.JwtAuthenticationFilter;
+import com.angelfg.spring_security_course.persistence.enums.RolePermission;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -30,13 +32,52 @@ public class HttpSecurityConfig {
                 .sessionManagement(sessMagConfig -> sessMagConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(daoAuthProvider) // Estrategia de autenticacion del provider
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(authReqConfig -> {
-                    authReqConfig.requestMatchers(HttpMethod.POST, "/customers").permitAll();
-                    authReqConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
-                    authReqConfig.requestMatchers(HttpMethod.GET, "/auth/validate-token").permitAll();
-                    authReqConfig.anyRequest().authenticated();
-                })
+                .authorizeHttpRequests(HttpSecurityConfig::buildRequestMatchers)
                 .build();
+    }
+
+    private static void buildRequestMatchers(
+            AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authReqConfig
+    ) {
+        /*
+        * Autorizacion de endpoints de products
+        */
+        authReqConfig.requestMatchers(HttpMethod.GET, "/products")
+                .hasAuthority(RolePermission.READ_ALL_PRODUCTS.name());
+        authReqConfig.requestMatchers(HttpMethod.GET, "/products/{productId}")
+                .hasAuthority(RolePermission.READ_ONE_PRODUCT.name());
+        authReqConfig.requestMatchers(HttpMethod.POST, "/products")
+                .hasAuthority(RolePermission.CREATE_ONE_PRODUCT.name());
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/products/{productId}")
+                .hasAuthority(RolePermission.UPDATE_ONE_PRODUCT.name());
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/products/{productId}/disabled")
+                .hasAuthority(RolePermission.DISABLE_ONE_PRODUCT.name());
+
+        /*
+        * Autorizacion de endpoints de categories
+        */
+        authReqConfig.requestMatchers(HttpMethod.GET, "/categories")
+                .hasAuthority(RolePermission.READ_ALL_CATEGORIES.name());
+        authReqConfig.requestMatchers(HttpMethod.GET, "/categories/{categoryId}")
+                .hasAuthority(RolePermission.READ_ONE_CATEGORY.name());
+        authReqConfig.requestMatchers(HttpMethod.POST, "/categories")
+                .hasAuthority(RolePermission.CREATE_ONE_CATEGORY.name());
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/categories/{categoryId}")
+                .hasAuthority(RolePermission.UPDATE_ONE_CATEGORY.name());
+        authReqConfig.requestMatchers(HttpMethod.PUT, "/categories/{categoryId}/disabled")
+                .hasAuthority(RolePermission.DISABLE_ONE_CATEGORY.name());
+
+        authReqConfig.requestMatchers(HttpMethod.GET, "/auth/profile")
+                .hasAuthority(RolePermission.READ_MY_PROFILE.name());
+
+        /*
+        * Autorizacion de endpoints publicos
+        */
+        authReqConfig.requestMatchers(HttpMethod.POST, "/customers").permitAll();
+        authReqConfig.requestMatchers(HttpMethod.POST, "/auth/authenticate").permitAll();
+        authReqConfig.requestMatchers(HttpMethod.GET, "/auth/validate-token").permitAll();
+
+        authReqConfig.anyRequest().authenticated();
     }
 
 }
